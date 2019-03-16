f00581dba717ef0e0d2c93a7f11027a88ed31fff
Divyendu Singh <divyendu.z@gmail.com>
2019-03-16 14:21:06 +0100

    fix(auth): only known group yield translation

--- backend-translation/src/user/index.ts
+++ backend-translation/src/user/index.ts
@@ -1,16 +1,6 @@
-import {makeInlineQueryResultArticle} from '../message';
+import { makeInlineQueryResultArticle } from "../message";
 
-const production = process.env.NODE_ENV === 'production' ? true : false;
-
-const betaTesters = [
-  'divyenduz',
-  'nilanm',
-  'rusrushal13',
-  'yuvika01',
-  'Lukastrong5',
-  'wilbertliu',
-];
-const knownUsers = production ? [...betaTesters] : [...betaTesters];
+const production = process.env.NODE_ENV === "production" ? true : false;
 
 export const addBotAccess = bot => {
   bot.use((ctx, next) => {
@@ -19,15 +9,15 @@ export const addBotAccess = bot => {
       ctx.answerInlineQuery(
         [
           makeInlineQueryResultArticle({
-            title: 'Join LanguageLearners to use the LingoParrot bot.',
-            description: 'https://languagelearners.club',
-            message: userNotKnownErrorMessage(ctx.from.username),
-          }),
+            title: "Join LanguageLearners to use the LingoParrot bot.",
+            description: "https://languagelearners.club",
+            message: userNotKnownErrorMessage(ctx.from.username)
+          })
         ],
         {
           is_personal: true,
-          cache_time: 0,
-        },
+          cache_time: 0
+        }
       );
       return;
     }
@@ -37,11 +27,28 @@ export const addBotAccess = bot => {
       ctx.message &&
       ctx.message.from &&
       ctx.message.chat &&
-      ctx.message.chat.type === 'private' &&
+      ctx.message.chat.type === "private" &&
       !isKnownUser(ctx.message.from.username)
     ) {
       console.log(`private chat but but unknown user`);
       ctx.reply(userNotKnownErrorMessage(ctx.from.username));
+      return;
+    }
+
+    if (
+      ctx.message &&
+      ctx.message.from &&
+      ctx.message.chat &&
+      ctx.message.chat.type === "group" &&
+      (!isKnownGroup(ctx.message.chat.title) ||
+        !isKnownUser(ctx.message.from.username))
+    ) {
+      if (!isKnownUser(ctx.message.from.username)) {
+        console.log(`unknown user: ${ctx.message.from.username}`);
+      }
+      if (!isKnownGroup(ctx.message.chat.title)) {
+        console.log(`unknown group: ${ctx.message.chat.title}`);
+      }
       return;
     }
 
@@ -51,10 +58,33 @@ export const addBotAccess = bot => {
 };
 
 const isKnownUser = userName => {
+  const developmentUsers = [
+    "divyenduz",
+    "nilanm",
+    "rusrushal13",
+    "yuvika01",
+    "Lukastrong5",
+    "wilbertliu"
+  ];
+  const productionUsers = []; // TODO: Fill this dynamically once payment workflow is done
+  const knownUsers = production
+    ? [...productionUsers, ...developmentUsers]
+    : [...developmentUsers];
   const index = knownUsers.indexOf(userName);
   return index > -1;
 };
+
+const isKnownGroup = groupName => {
+  const developmentGroups = ["Development - Language Learners Club"];
+  const productionGroups = ["German - Language Learners Club"];
+  const knownGroups = production
+    ? [...productionGroups]
+    : [...developmentGroups];
+  const index = knownGroups.indexOf(groupName);
+  return index > -1;
+};
+
 const userNotKnownErrorMessage = userName => {
-  const joinLink = 'https://languagelearners.club';
+  const joinLink = "https://languagelearners.club";
   return `User ${userName} is not indentified. You need to join ${joinLink} before using LingoParrot.`;
 };
